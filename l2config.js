38ba05846b541a33095ebc3a2a84f36f55473d3c
communityboard <dev@skogorev.com>
2014-12-08 22:04:21 +0300

    Update l2config.js

--- l2config.js
+++ l2config.js
@@ -1,18 +1,18 @@
 {
-	"title": "Example Schema",
-	"type": "object",
-	"properties": {
-		"firstName": {
-			"type": "string"
-		},
-		"lastName": {
-			"type": "string"
-		},
-		"age": {
-			"description": "Age in years",
-			"type": "integer",
-			"minimum": 0
-		}
-	},
-	"required": ["firstName", "lastName"]
+	"version": {
+		"Goddess of Destruction" : "http://files.slineage.com/sLineageIIHF5Euro.rar",
+		"High Five" : "http://files.slineage.com/sLineageIIHF5Euro.rar",
+		"Freya" : "http://files.slineage.com/sLineageIIHF5Euro.rar",
+		"Gracia Epilogue" : "http://files.slineage.com/sLineageIIHF5Euro.rar",
+		"Gracia Final" : "http://files.slineage.com/sLineageIIHF5Euro.rar",
+		"Gracia" : "http://files.slineage.com/sLineageIIHF5Euro.rar",
+		"Hellbound" : "http://files.slineage.com/sLineageIIHF5Euro.rar",
+		"Kamael" : "http://files.slineage.com/sLineageIIHF5Euro.rar",
+		"Interlude" : "http://files.slineage.com/sLineageIIHF5Euro.rar",
+		"C5" : "http://files.slineage.com/sLineageIIHF5Euro.rar",
+		"C4" : "http://files.slineage.com/sLineageIIHF5Euro.rar",
+		"C3" : "http://files.slineage.com/sLineageIIHF5Euro.rar",
+		"C2" : "http://files.slineage.com/sLineageIIHF5Euro.rar",
+		"C1" : "http://files.slineage.com/sLineageIIHF5Euro.rar"
+	}
 }
